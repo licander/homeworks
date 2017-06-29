cc2c3739a288bd267d3b387d0a27cae448ce7527
licander <licand@mail.ru>
2017-06-30 00:04:59 +0300

    Add files via upload

--- homework2.3.py
+++ homework2.3.py
@@ -13,7 +13,28 @@ def read_from_file(file_name):
 def find_popular_words(list_of_data):
     dictionary = {}
     for new in list_of_data:
-        print(new['title'])
+        text_list = new['title'].split() + new['description'].split()
+        for word in text_list:
+            if len(word) > 6:
+                if word in dictionary:
+                    dictionary[word] += 1
+                else:
+                   dictionary[word] = 1
+    l = lambda x: x[1]
+    word_sort_list = sorted(dictionary.items(), key=l, reverse=True)
+    return word_sort_list
 
-data = read_from_file('newsafr.json')
-find_popular_words(data)
+def print_results(word_sort_list):
+    for i in range(0,10):
+        print(word_sort_list[i])
+
+def main(file_name):        
+    data = read_from_file(file_name)
+    word_sort_list = find_popular_words(data)
+    print('Результаты для файла {}'.format(file_name))
+    print_results(word_sort_list)
+    
+main('newsafr.json')
+main('newscy.json')
+main('newsfr.json')
+main('newsit.json')
