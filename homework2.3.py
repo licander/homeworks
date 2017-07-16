ba42d8723ccd571996532fcf445c57ef1ea53397
licander <licand@mail.ru>
2017-07-16 15:05:04 +0300

    Add files via upload

--- homework2.3.py
+++ homework2.3.py
@@ -2,14 +2,16 @@
 import chardet
 import json
 
+
 def read_from_file(file_name):
     with open(file_name, 'rb') as file:
         data = file.read()
         coding = chardet.detect(data)
-        data = data.decode(coding['encoding'])         
+        data = data.decode(coding['encoding'])
         data = json.loads(data)
     return data['rss']['channel']['items']
-        
+
+
 def find_popular_words(list_of_data):
     dictionary = {}
     for new in list_of_data:
@@ -19,21 +21,24 @@ def find_popular_words(list_of_data):
                 if word in dictionary:
                     dictionary[word] += 1
                 else:
-                   dictionary[word] = 1
-    l = lambda x: x[1]
+                    dictionary[word] = 1
+
+    def l(x): return x[1]
     word_sort_list = sorted(dictionary.items(), key=l, reverse=True)
     return word_sort_list
 
+
 def print_results(word_sort_list):
-    for i in range(0,10):
+    for i in range(0, 10):
         print(word_sort_list[i])
 
-def main(file_name):        
+
+def main(file_name):
     data = read_from_file(file_name)
     word_sort_list = find_popular_words(data)
     print('Результаты для файла {}'.format(file_name))
     print_results(word_sort_list)
-    
+
 main('newsafr.json')
 main('newscy.json')
 main('newsfr.json')
